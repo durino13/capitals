05f88590b1f8fd67b57287bc54ae377f5c3f6ae1
Juraj Marusiak <juraj.marusiak@gmail.com>
2016-04-24 21:13:59 +0200

    Changed app title ..

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Fixed Layout</title>
+    <title>World capitals game by Juraj Marušiak</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
 </head>
